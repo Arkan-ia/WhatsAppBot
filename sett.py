c6e9ef01e11f5801e788553a1db00fbea6ebd3d7
Brayan G. Moreno <107154065+bgarzonm@users.noreply.github.com>
2024-10-16 20:45:43 -0500

    Update sett.py

--- sett.py
+++ sett.py
@@ -1,3 +1,3 @@
-token = '#CLAVE PARA EL WEBHOOK'
-whatsapp_token = '#TOKEN'
-whatsapp_url = 'https://graph.facebook.com/v20.0/#####/messages'
+token = 'spiralabs'
+whatsapp_token = 'EAAGgx3lKGfQBO4YUQ7hLSxVzbZBMH2gvgGAHay0sdnKu1gjsUib1Hx41lJHPcN6nZBJqHuNvzOUYJfPPXmCQo8iHcgJtuC4mCZAnCqmXWRXxuZCLKHZC7TviMewAZCQ4FNgNmRMJYxtZCBKoFw2sT99rKLmqAju1CHGE79rkKRsZCcj1VA4d4G3l22vOAK1kKTnfrXO42sW2ZAJByaKWG0royZCphQXmsvLyAjMk35EqIZD'
+whatsapp_url = 'https://graph.facebook.com/v20.0/400692489794103/messages'
