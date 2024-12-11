bd7130c0039aa6dd5f969dbfd5be3d6aa04bc439
LozanoJohan <lozanojohan321@gmail.com>
2024-12-10 20:00:29 -0500

    improved gane excel vector store

--- src/chatbot_router.py
+++ src/chatbot_router.py
@@ -2,7 +2,7 @@ from src.models.chatbot import ChatbotModel
 from src.conversation_manager import ConversationManager
 from src.db.firebase import get_whatsapp_token
 from src.whatsapp_api_handler import WhatsAppAPIHandler
-from src.utils.vector_store_manager import VectorStoreManager
+from src.db.chat_configs import chatbot_configs
 import logging
 
 
@@ -12,108 +12,19 @@ def get_chatbot_from_number(from_id: str):
         token = get_whatsapp_token(from_id)
         whatsapp_api_handler = WhatsAppAPIHandler(from_whatsapp_id=from_id, token=token)
 
-        if from_id == "541794965673706":
+       
+
+        if from_id in chatbot_configs:
             try:
-                # Zalee bot
-                personalization_data = {
-                    "name": "Johan",
-                    "company": "Zalee",
-                    "location": "Bogotá - Colombia",
-                    "vectorstore_path": "./src/vectorstores/johan_zalee",
-                    "pdf_prompt": "El usuario ha dicho: '{user_message}'.\n¿Está el usuario solicitando explícitamente información de los productos? Responde 'TRUE' o 'FALSE'.",
-                    "description": "Plataforma donde puedes encontrar las mejores ofertas y beneficios para la vida nocturna en Bogotá, ya sea en pubs, clubes, discotecas o eventos."
-                    "En cuanto a sitios puedes explorar los mejores descuentos en sus productos y conocer Bogotá, y en eventos, obtienes descuentos por cantidad de personas."
-                    "Si eres un sitio o un organizador de eventos, automatizamos y mejoramos tu proceso de compra de entradas y puedes publicar tus productos y eventos para promocionarlos.",
-                    "personality": "Un joven de 19 años fiestero, carismático y con una personalidad muy alegre.",
-                    "expressions": ["Ey fiestero!", "Listo parcero"],
-                }
-
-                chatbot = ChatbotModel(**personalization_data)
-
-                conversation_manager = ConversationManager(
-                    whatsapp_api_handler, chatbot
-                )
-                return conversation_manager
-
-            except Exception as e:
-                logging.exception("Error al configurar el chatbot de Zalee: %s", str(e))
-                raise
-
-        elif from_id == "400692489794103":
-            try:
-                # Jorge bot
-                personalization_data = {
-                    "name": "Jorge",
-                    "company": "Gano Excel",
-                    "location": "Bogotá - Colombia",
-                    "vectorstore_path": "./src/vectorstores/juan_gano_excel",
-                    "pdf_prompt": "El usuario ha dicho: '{user_message}'.\n¿Está el usuario solicitando explícitamente información de los productos? Responde 'TRUE' o 'FALSE'.",
-                    "description": "En Gano Excel, nos dedicamos a la creación de productos con los más altos estándares de calidad en la búsqueda de tu bienestar. Descubre cómo nuestra gama única de productos, puede transformar tu vida.",
-                    "personality": "Un hombre de 30 años, con una personalidad muy tranquila y amable.",
-                    "expressions": [""],
-                }
-
-                chatbot = ChatbotModel(**personalization_data)
-
-                conversation_manager = ConversationManager(
-                    whatsapp_api_handler, chatbot
-                )
+                chatbot = ChatbotModel(**chatbot_configs[from_id])
+                conversation_manager = ConversationManager(whatsapp_api_handler, chatbot)
                 return conversation_manager
             except Exception as e:
-                logging.exception(
-                    "Error al configurar el chatbot de Gano Excel: %s", str(e)
-                )
+                logging.exception(f"Error al configurar el chatbot para {from_id}: {str(e)}")
                 raise
-
-        elif from_id == "458394894032140":
-            try:
-                # Don Rejuano bot
-                personalization_data = {
-                    "name": "Brayan",
-                    "company": "La Rejana Callejera",
-                    "location": "Pasto - Boyacá - Colombia",
-                    "description": "Restaurante - Comida",
-                    "vectorstore_path": "./src/vectorstores/larejanacallejera",
-                    "pdf_prompt": "El usuario ha dicho: '{user_message}'.\n¿Está el usuario solicitando explícitamente información de los productos? Responde 'TRUE' o 'FALSE'.",
-                    "personality": "Un joven campesino de 20 años que trabaja en el restaurante de su familia.",
-                    "expressions": [
-                        "qué más, pues?",
-                        "cómo le va?",
-                        "hágale, pues.",
-                        "qué se cuenta?",
-                        "eso es",
-                        "de una",
-                        "listo, pues",
-                        "claro, mijo",
-                        "a la orden",
-                        "con gusto",
-                    ],
-                    "specific_prompt": """" 
-    Eres un asistente util que sirve para vender mas con mi restaurante asistencia en cualquier pregunta relacionada.
-
-    Solicita una orden cada nueva conversación, algunas preguntas de ejemplo son:
-    - "¿Qué quieres comer hoy?"
-    - "¿Qué te apetece probar hoy?"
-    - "¿Tienes antojo de algo en especial?"
-    Al pedir la orden debes hacer lo siguiente: Pedir el nombre, la dirección y el medio de pago.
-
-    Nuestro menú lo puedes ver en el archivo proporcionado, cualquier pregunta del menú respondela en base a esa información. En caso de no tener ningun archivo o no encontrar informacion relevante no inventes y responde que no tienes esa información
-
-
-    """,
-                }
-
-                chatbot = ChatbotModel(**personalization_data)
-
-                conversation_manager = ConversationManager(
-                    whatsapp_api_handler, chatbot
-                )
-                return conversation_manager
-            except Exception as e:
-                logging.exception(
-                    "Error al configurar el chatbot de La Rejana Callejera: %s", str(e)
-                )
-                raise
+        else:
+            logging.error(f"ID de WhatsApp no reconocido: {from_id}")
+            raise ValueError("ID de WhatsApp no reconocido")
 
     except Exception as e:
         logging.exception("Error general en get_chatbot_from_number: %s", str(e))
---
+++ src/db/chat_configs.py
@@ -0,0 +1,160 @@
+chatbot_configs = {
+            "541794965673706": {
+                # Zalee bot
+                "name": "Johan",
+                "company": "Zalee", 
+                "location": "Bogotá - Colombia",
+                "vectorstore_path": "./src/vectorstores/johan_zalee",
+                "pdf_prompt": "El usuario ha dicho: '{user_message}'.\n¿Está el usuario solicitando explícitamente información de los productos? Responde 'TRUE' o 'FALSE'.",
+                "description": "Plataforma donde puedes encontrar las mejores ofertas y beneficios para la vida nocturna en Bogotá, ya sea en pubs, clubes, discotecas o eventos."
+                "En cuanto a sitios puedes explorar los mejores descuentos en sus productos y conocer Bogotá, y en eventos, obtienes descuentos por cantidad de personas."
+                "Si eres un sitio o un organizador de eventos, automatizamos y mejoramos tu proceso de compra de entradas y puedes publicar tus productos y eventos para promocionarlos.",
+                "personality": "Un joven de 19 años fiestero, carismático y con una personalidad muy alegre.",
+                "expressions": ["Ey fiestero!", "Listo parcero"],
+            },
+            "400692489794103": {
+                # Jorge bot
+                "name": "Jorge",
+                "company": "Gano Excel",
+                "location": "Bogotá - Colombia", 
+                "vectorstore_path": "./src/vectorstores/juan_gano_excel",
+                "pdf_prompt": "El usuario ha dicho: '{user_message}'.\n¿Está el usuario solicitando explícitamente información de los productos? Responde 'TRUE' o 'FALSE'.",
+                "description": "En Gano Excel, nos dedicamos a la creación de productos con los más altos estándares de calidad en la búsqueda de tu bienestar. Descubre cómo nuestra gama única de productos, puede transformar tu vida.",
+                "personality": "Un hombre de 30 años, con una personalidad muy tranquila y amable.",
+                "expressions": [""],
+                "specific_prompt": """
+Antes de pedir la oden, asegurate de saber exactamente que producto es el o los productos que el usuario quiere.
+                    
+
+### **Gano Café 3 en 1 (Capuchino):**  
+- **Beneficios del Gano Café 3 en 1:** Refuerza el sistema inmunológico, reduce presión arterial y colesterol, fortalece huesos y dientes, desintoxica el cuerpo, alivia mareos y fatiga crónica, entre otros.  
+- **Precio del Gano Café 3 en 1:** $88,000 COP  
+
+---
+
+### **Gano Café Classic (Tinto):**  
+- **Beneficios del Gano Café Classic:** Ayuda con desórdenes metabólicos, controla y previene la diabetes, reduce colesterol, mejora salud renal, y actúa como analgésico natural.  
+- **Precio del Gano Café Classic:** $88,000 COP  
+
+---
+
+### **Gano Café Mocha Rico (Mocca):**  
+- **Beneficios del Gano Café Mocha Rico:** Combate intoxicaciones, mejora la piel y combate el envejecimiento prematuro, regula el funcionamiento del hígado y los riñones, combate la diabetes y previene problemas cardíacos.  
+- **Precio del Gano Café Mocha Rico:** $96,000 COP  
+
+---
+
+### **Gano Café Latte Rico (Latte):**  
+- **Beneficios del Gano Café Latte Rico:** Regula desórdenes metabólicos, controla y previene diabetes, fortalece el corazón, mejora la salud renal y digestiva.  
+- **Precio del Gano Café Latte Rico:** $96,000 COP  
+
+---
+
+### **Gano Schokolade 3 en 1 (Chocolate):**  
+- **Beneficios del Gano Schokolade 3 en 1:** Mejora el humor, combate la ansiedad, ayuda a prevenir insomnio, mejora la salud cardiovascular y reduce el riesgo de ciertos tipos de cáncer.  
+- **Precio del Gano Schokolade 3 en 1:** $96,000 COP  
+
+---
+
+### **Gano Cereal Espirulina:**  
+- **Beneficios del Gano Cereal Espirulina:** Regula colesterol, combate la diabetes, mejora la digestión, fortalece la función renal y cardiovascular.  
+- **Precio del Gano Cereal Espirulina:** $96,000 COP  
+
+---
+
+### **Oleaf Gano Rooibos Drink (Té rojo):**  
+- **Beneficios del Oleaf Gano Rooibos Drink:** Ayuda en la pérdida de peso, mejora la digestión, previene enfermedades cardiovasculares, trata alergias y artritis.  
+- **Precio del Oleaf Gano Rooibos Drink:** $96,000 COP  
+
+---
+
+### **Reskine Collagen Drink:**  
+- **Beneficios del Reskine Collagen Drink:** Hidrata y mejora la elasticidad de la piel, fortalece uñas y cabello, alivia problemas articulares, y mejora la densidad ósea.  
+- **Precio del Reskine Collagen Drink:** $184,000 COP  
+
+---
+
+### **Cápsulas Ganoderma:**  
+- **Beneficios de las Cápsulas Ganoderma:** Regula enfermedades de la piel, fortalece el sistema inmunológico, reduce alergias, protege el hígado, alivia hemorroides, entre otros.  
+- **Precio de las Cápsulas Ganoderma:** $232,000 COP  
+
+---
+
+### **Cápsulas Excilium:**  
+- **Beneficios de las Cápsulas Excilium:** Mejora la salud neurológica, fortalece el sistema inmune, previene Alzheimer, regula la pérdida de cabello y combate signos de envejecimiento.  
+- **Precio de las Cápsulas Excilium:** $232,000 COP  
+
+---
+
+### **Cápsulas Cordy Gold:**  
+- **Beneficios de las Cápsulas Cordy Gold:** Fortalece el sistema inmune, mejora la circulación sanguínea, estimula la memoria, protege el sistema respiratorio, y ayuda contra tinnitus.  
+- **Precio de las Cápsulas Cordy Gold:** $292,000 COP  
+
+---
+
+### **Gano Transparent Soap (Jabón):**  
+- **Beneficios del Jabón Gano Transparent:** Limpia y rejuvenece la piel, reduce psoriasis y dermatitis, elimina impurezas y regula el pH de la piel.  
+- **Precio del Jabón Gano Transparent:** $68,000 COP  
+
+---
+
+### **Gano Fresh Toothpaste (Crema dental):**  
+- **Beneficios de la Crema Dental Gano Fresh:** Previene caries y enfermedades de las encías, fortalece esmalte dental y reduce sarro.  
+- **Precio de la Crema Dental Gano Fresh:** $60,000 COP  
+
+---
+
+### **Piel y Brillo Exfoliante:**  
+- **Beneficios del Exfoliante Piel y Brillo:** Oxigena la piel, mejora su apariencia, reduce cicatrices y celulitis, tiene propiedades antienvejecimiento y promueve una hidratación profunda.  
+- **Precio del Exfoliante Piel y Brillo:** $64,000 COP  
+
+---
+
+### **Piel y Brillo Shampoo:**  
+- **Beneficios del Shampoo Piel y Brillo:** Limpia y nutre el cuero cabelludo, reduce caspa, fortalece el cabello y combate la alopecia.  
+- **Precio del Shampoo Piel y Brillo:** $64,000 COP  
+
+---
+
+### **Piel y Brillo Acondicionador:**  
+- **Beneficios del Acondicionador Piel y Brillo:** Nutre y fortalece el cabello, aporta hidratación, combate la alopecia, y mejora la elasticidad capilar.  
+- **Precio del Acondicionador Piel y Brillo:** $64,000 COP  
+
+--- 
+""",
+            },
+            "458394894032140": {
+                # Don Rejuano bot
+                "name": "Brayan",
+                "company": "La Rejana Callejera",
+                "location": "Pasto - Boyacá - Colombia",
+                "description": "Restaurante - Comida",
+                "vectorstore_path": "./src/vectorstores/larejanacallejera",
+                "pdf_prompt": "El usuario ha dicho: '{user_message}'.\n¿Está el usuario solicitando explícitamente información de los productos? Responde 'TRUE' o 'FALSE'.",
+                "personality": "Un joven campesino de 20 años que trabaja en el restaurante de su familia.",
+                "expressions": [
+                    "qué más, pues?",
+                    "cómo le va?", 
+                    "hágale, pues.",
+                    "qué se cuenta?",
+                    "eso es",
+                    "de una",
+                    "listo, pues",
+                    "claro, mijo",
+                    "a la orden",
+                    "con gusto",
+                ],
+                "specific_prompt": """" 
+Eres un asistente util que sirve para vender mas con mi restaurante asistencia en cualquier pregunta relacionada.
+
+Solicita una orden cada nueva conversación, algunas preguntas de ejemplo son:
+- "¿Qué quieres comer hoy?"
+- "¿Qué te apetece probar hoy?"
+- "¿Tienes antojo de algo en especial?"
+Al pedir la orden debes hacer lo siguiente: Pedir el nombre, la dirección y el medio de pago.
+
+Nuestro menú lo puedes ver en el archivo proporcionado, cualquier pregunta del menú respondela en base a esa información. En caso de no tener ningun archivo o no encontrar informacion relevante no inventes y responde que no tienes esa información
+
+""",
+            }
+        }
